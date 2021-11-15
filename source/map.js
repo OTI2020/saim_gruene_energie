2753d1f3f314c113c2fb1123a6b7e9d6499123ef
OTI2020 <61976211+OTI2020@users.noreply.github.com>
2021-11-15 14:57:22 +0100

    add basemap from leaflet

---
+++ source/map.js
@@ -0,0 +1,20 @@
+'use strict'
+/**
+ * Basemap for Leaflet Application
+ * @type Leaflet Map
+ */
+    let mymap = L.map('mapid').setView([51.965, 7.63],13);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mymap);
+    
+
+/**
+ * Layer for later addition of markers
+ * @type Leaflet Layer
+ */
+    let markerLayer = new L.layerGroup().addTo(mymap);
+
+    // call getDatafromDB() to load existing pois at page load
+    // getDatafromDB();
+    // 
