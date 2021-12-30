379974988da64bb779780202589e03b8099eee8e
Timo Lietmeyer <48100977+Timo123456789@users.noreply.github.com>
2021-12-30 13:45:38 +0100

    add data, first try to add leaflet map

--- source/map.js
+++ source/map.js
@@ -3,12 +3,16 @@
  * Basemap for Leaflet Application
  * @type Leaflet Map
  */
+
+ var map = L.map('map').setView([51.505, -0.09], 13);
+
+ 
     let mymap = L.map('mapID').setView([51.965, 7.63],13);
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mymap);
     
-
+console.log("in Leafletjs");
 /**
  * Layer for later addition of markers
  * @type Leaflet Layer
